25bf422bc8cc52eeeaa256a10929dc0616fe4d45
simonLab <simon.labondance@gmail.com>
2018-06-17 18:22:00 +0100

    update package.json

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 var express = require("express");
 var path = require("path");
 var hbs = require("hbs");
+var logger = require("morgan");
 
 var indexRouter = require("./routes/index.js");
 
 var app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "hbs");
-
+app.use(logger("dev"));
 app.use("/", indexRouter);
 
 module.exports = app;
